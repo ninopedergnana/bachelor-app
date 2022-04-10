8cf858f9ad1d3b8d84760d7a55158975f27c8658
pedernin <pedernin@students.zhaw.ch>
2022-04-10 16:57:46 +0200

    sign up and key generation

---
+++ lib/models/Person.dart
@@ -0,0 +1,15 @@
+class Person {
+  String firstname;
+  String lastname;
+  String email;
+  String passphrase;
+
+  Person({
+    required this.firstname,
+    required this.lastname,
+    required this.email,
+    required this.passphrase
+  });
+
+
+}
