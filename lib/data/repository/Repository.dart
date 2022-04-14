9b5ab686055b29642ff3f17c739ae86db7cd9345
pedernin <pedernin@students.zhaw.ch>
2022-04-14 16:26:06 +0200

    update

--- lib/data/repository/Repository.dart
+++ lib/data/repository/Repository.dart
@@ -7,7 +7,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:openpgp/openpgp.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart';
-import 'package:eth_sig_util/eth_sig_util.dart';
 
 // TODO: Convert to singleton
 class Repository {
@@ -15,40 +14,53 @@ class Repository {
   late Impfy _client;
   final EthereumAddress _contractAddress =
       EthereumAddress.fromHex('0xb58d3d11966CCeB725e39C3d6D0d383Bf3F1cec3');
-  final String _rpcUrl = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+  final String _rpcUrl =
+      'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
 
   Repository() {
     var client = Web3Client(_rpcUrl, Client());
     _client = Impfy(address: _contractAddress, client: client);
   }
 
- // Future<Map<String, Certificate>> getCertificates() async {
- //   var passphrase = '';
- //   String privateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
- //   Credentials credentials = EthPrivateKey.fromHex(privateKey);
- //   EthereumAddress address = await credentials.extractAddress();
- //   var result = await _client.getCertificates(address);
- //   var x = { for (var e in result) e['certificateHash'] as String : decryptCertificate(e, privateKey, passphrase) };
- //   Map<String, Certificate> certificates = await Future.wait(x);
- //   return certificates;
- // }
+  Future<List<Certificate>> getCertificates() async {
+    String passphrase = '';
 
-  Future<void> createCertificate(Certificate certificate, PatientKeysDTO patientKeys) async {
-    String doctorKey = (await _localStorage.read(key: 'pgpPublicKey'))!;
-    String privateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
-    Credentials credentials = EthPrivateKey.fromHex(privateKey);
+    // ethereum privat key fetched, and public key generated with credentials
+    String ethPrivateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
+    Credentials credentials = EthPrivateKey.fromHex(ethPrivateKey);
+    EthereumAddress address = await credentials.extractAddress();
+
+    // result structure currently unknown
+    var result = (await _client.getCertificates(address));
+    var certs = result.map((element) {
+      return List.from(element);
+    });
+
+    var x = certs.map((e) => e.first).map((e) => decryptCertificate(e, passphrase));
+
+    return Future.wait(List.from(x));
+  }
+
+  Future<void> createCertificate(
+      Certificate certificate, PatientKeysDTO patientKeys) async {
+    String pgpDoctorPublicKey =
+        (await _localStorage.read(key: 'pgpPublicKey'))!;
+    String ethPrivateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
+    Credentials credentials = EthPrivateKey.fromHex(ethPrivateKey);
     String hash = certificate.hashCode.toString();
     // String signedHash = EthSigUtil.signTypedData(
     //     privateKey: privateKey, jsonData: json.encode({"hash": hash}), version: TypedDataVersion.V4);
-    String encryptedCertificate = await certificate.encrypt(doctorKey, patientKeys.pgpKey);
-    _client.addCertificate(
-        encryptedCertificate, hash, EthereumAddress.fromHex(patientKeys.ethAddress),
+    String encryptedCertificate =
+        await certificate.encrypt(pgpDoctorPublicKey, patientKeys.pgpKey);
+    _client.addCertificate(encryptedCertificate, hash,
+        EthereumAddress.fromHex(patientKeys.ethAddress),
         credentials: credentials);
   }
 
-  Future<Certificate> decryptCertificate(dynamic data, String privateKey, String passphrase) async {
-    String certificateJson =
-        await OpenPGP.decrypt(data['encryptedCertificate'], privateKey, passphrase);
+  Future<Certificate> decryptCertificate(String data, String passphrase) async {
+    String privateKey = (await _localStorage.read(key: 'pgpPrivateKey'))!;
+    String certificateJson = await OpenPGP.decrypt(
+        data, privateKey, passphrase);
     return Certificate.fromJson(jsonDecode(certificateJson));
   }
 }
--- lib/presentation/screens/CertificateList.dart
+++ lib/presentation/screens/CertificateList.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/domain/model/Certificate.dart';
 import 'package:flutter_app/presentation/screens/CertificateDetail.dart';
 
+import '../../data/repository/Repository.dart';
+
 class CertificateList extends StatelessWidget {
-  const CertificateList({Key? key, required this.vaccineList}) : super(key: key);
+  CertificateList({Key? key, required this.vaccineList}) : super(key: key);
 
   final List<Certificate> vaccineList;
+  final Repository repo = Repository();
 
   @override
   Widget build(BuildContext context) {
+    var vaccs = repo.getCertificates().then((e) {
+      e.forEach((element) {print(element.toString());});
+    });
     return Scaffold(
       appBar: AppBar(
         title: const Text('List of Vaccines'),
