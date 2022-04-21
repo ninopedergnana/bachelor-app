81a17c860b14237b33ad56249060805092838a37
Carlos Olgiati <olgiacar@students.zhaw.ch>
2022-04-21 09:30:23 +0200

    Added ipfs api calls Made Repository singleton Updated abi and impfy.g.dart

--- lib/data/repository/Repository.dart
+++ lib/data/repository/Repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
+import 'package:flutter_app/data/api/ipfs.dart';
 import 'package:flutter_app/data/dto/PatientDTO.dart';
 import 'package:flutter_app/domain/model/Certificate.dart';
 import 'package:flutter_app/domain/model/SignedCertificate.dart';
@@ -12,31 +13,28 @@ import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart';
 import 'package:eth_sig_util/eth_sig_util.dart';
 
-// TODO: Convert to singleton
 class Repository {
-  final FlutterSecureStorage _localStorage = const FlutterSecureStorage();
-  late Impfy _client;
-  final EthereumAddress _contractAddress =
-      EthereumAddress.fromHex('0xb58d3d11966CCeB725e39C3d6D0d383Bf3F1cec3');
-  final String _rpcUrl = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+  static final Repository _instance = Repository._internal();
+  final IPFS _ipfs = IPFS();
+  final FlutterSecureStorage _secureStore = const FlutterSecureStorage();
+  late final Impfy _impfy;
 
-  Repository() {
-    var client = Web3Client(_rpcUrl, Client());
-    _client = Impfy(address: _contractAddress, client: client);
+  factory Repository() {
+    return _instance;
+  }
+
+  Repository._internal() {
+    String hexAddress = '0x926684114C466a2D6Ad87292fEC30E98aC8e0196';
+    EthereumAddress contractAddress = EthereumAddress.fromHex(hexAddress);
+    String rpcUrl = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+    var client = Web3Client(rpcUrl, Client());
+    _impfy = Impfy(address: contractAddress, client: client);
   }
 
   Future<List<SignedCertificate>> getCertificates() async {
     String passphrase = '';
 
-    // ethereum privat key fetched, and public key generated with credentials
-    String ethPrivateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
-    Credentials credentials = EthPrivateKey.fromHex(ethPrivateKey);
-    // EthereumAddress address = await credentials.extractAddress();
-    // FIXME: Remove
-    EthereumAddress address = EthereumAddress.fromHex('0x94e5999Dea8b2369fd313C34EE4ab2217E6F7B10');
-
-    // result structure currently unknown
-    var response = (await _client.getCertificates(address));
+    var response = (await _impfy.getCertificates());
     var certificates = response
         .map((element) => List.from(element))
         .map((element) => _decryptCertificate(element, passphrase));
@@ -45,23 +43,23 @@ class Repository {
   }
 
   Future<void> createCertificate(Certificate certificate, PatientDTO patient) async {
-    String pgpDoctorPublicKey = (await _localStorage.read(key: 'pgpPublicKey'))!;
-    String ethPrivateKey = (await _localStorage.read(key: 'ethPrivateKey'))!;
+    String pgpDoctorPublicKey = (await _secureStore.read(key: 'pgpPublicKey'))!;
+    String ethPrivateKey = (await _secureStore.read(key: 'ethPrivateKey'))!;
     Credentials credentials = EthPrivateKey.fromHex(ethPrivateKey);
     String hash = md5.convert(certificate.toString().codeUnits).toString();
     String signedHash = EthSigUtil.signMessage(
         privateKey: ethPrivateKey, message: Uint8List.fromList(hash.codeUnits));
     String encryptedCertificate =
         await certificate.encrypt(pgpDoctorPublicKey, patient.pgpPublicKey);
-    _client.addCertificate(
-        encryptedCertificate, signedHash, EthereumAddress.fromHex(patient.ethAddress),
+    String ipfsHash = await _ipfs.postCertificate(encryptedCertificate);
+    _impfy.addCertificate(signedHash, ipfsHash, EthereumAddress.fromHex(patient.ethAddress),
         credentials: credentials);
   }
 
   Future<SignedCertificate?> _decryptCertificate(List<dynamic> data, String passphrase) async {
     String encryptedCertificate = data.first as String;
     String signedHash = data.last as String;
-    String privateKey = (await _localStorage.read(key: 'pgpPrivateKey'))!;
+    String privateKey = (await _secureStore.read(key: 'pgpPrivateKey'))!;
     try {
       String certificateJson = await OpenPGP.decrypt(encryptedCertificate, privateKey, passphrase);
       Certificate certificate = Certificate.fromJson(json.decode(certificateJson));
--- lib/impfy.g.dart
+++ lib/impfy.g.dart
@@ -3,7 +3,7 @@
 import 'package:web3dart/web3dart.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"doctor","type":"address"}],"name":"addDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"doctor","type":"address"}],"name":"removeDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"encryptedCertificate","type":"string"},{"internalType":"string","name":"certificateHash","type":"string"},{"internalType":"address","name":"patient","type":"address"}],"name":"addCertificate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"patient","type":"address"}],"name":"getCertificates","outputs":[{"components":[{"internalType":"string","name":"encryptedCertificate","type":"string"},{"internalType":"string","name":"certificateHash","type":"string"}],"internalType":"struct Impfy.Vaccine[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function","constant":true}]',
+    '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"certificateHash","type":"string"},{"indexed":false,"internalType":"address","name":"doctor","type":"address"},{"indexed":false,"internalType":"address","name":"patient","type":"address"}],"name":"Vaccine","type":"event"},{"inputs":[{"internalType":"address","name":"doctor","type":"address"}],"name":"addDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"certificateHash","type":"string"},{"internalType":"string","name":"ipfsHash","type":"string"},{"internalType":"address","name":"patient","type":"address"}],"name":"addCertificate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getCertificates","outputs":[{"components":[{"internalType":"string","name":"certificateHash","type":"string"},{"internalType":"string","name":"ipfsHash","type":"string"},{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"internalType":"struct Impfy.CertificateDTO[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"doctor","type":"address"}],"name":"invalidateDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"certificateHash","type":"string"}],"name":"invalidateCertificate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"certificateHash","type":"string"}],"name":"isValidCertificate","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]',
     'Impfy');
 
 class Impfy extends _i1.GeneratedContract {
@@ -28,11 +28,35 @@ class Impfy extends _i1.GeneratedContract {
   /// The optional [transaction] parameter can be used to override parameters
   /// like the gas price, nonce and max gas. The `data` and `to` fields will be
   /// set by the contract.
-  Future<String> removeDoctor(_i1.EthereumAddress doctor,
+  Future<String> addCertificate(
+      String certificateHash, String ipfsHash, _i1.EthereumAddress patient,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
     final function = self.abi.functions[2];
-    assert(checkSignature(function, '98fc90e9'));
+    assert(checkSignature(function, '362846dc'));
+    final params = [certificateHash, ipfsHash, patient];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<List<dynamic>> getCertificates({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[3];
+    assert(checkSignature(function, '1f5ddb4f'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as List<dynamic>).cast<dynamic>();
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> invalidateDoctor(_i1.EthereumAddress doctor,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[4];
+    assert(checkSignature(function, 'b86a9c5e'));
     final params = [doctor];
     return write(credentials, transaction, function, params);
   }
@@ -40,25 +64,49 @@ class Impfy extends _i1.GeneratedContract {
   /// The optional [transaction] parameter can be used to override parameters
   /// like the gas price, nonce and max gas. The `data` and `to` fields will be
   /// set by the contract.
-  Future<String> addCertificate(String encryptedCertificate,
-      String certificateHash, _i1.EthereumAddress patient,
+  Future<String> invalidateCertificate(String certificateHash,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[3];
-    assert(checkSignature(function, '362846dc'));
-    final params = [encryptedCertificate, certificateHash, patient];
+    final function = self.abi.functions[5];
+    assert(checkSignature(function, '24f349f9'));
+    final params = [certificateHash];
     return write(credentials, transaction, function, params);
   }
 
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<List<dynamic>> getCertificates(_i1.EthereumAddress patient,
+  Future<bool> isValidCertificate(String certificateHash,
       {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[4];
-    assert(checkSignature(function, 'f1c49c62'));
-    final params = [patient];
+    final function = self.abi.functions[6];
+    assert(checkSignature(function, 'b5164b38'));
+    final params = [certificateHash];
     final response = await read(function, params, atBlock);
-    return (response[0] as List<dynamic>).cast<dynamic>();
+    return (response[0] as bool);
   }
+
+  /// Returns a live stream of all Vaccine events emitted by this contract.
+  Stream<Vaccine> vaccineEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('Vaccine');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return Vaccine(decoded);
+    });
+  }
+}
+
+class Vaccine {
+  Vaccine(List<dynamic> response)
+      : certificateHash = (response[0] as String),
+        doctor = (response[1] as _i1.EthereumAddress),
+        patient = (response[2] as _i1.EthereumAddress);
+
+  final String certificateHash;
+
+  final _i1.EthereumAddress doctor;
+
+  final _i1.EthereumAddress patient;
 }
